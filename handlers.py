d3dfc7c656b7a402e41c46d760de65dcb8a3f1b3
feison <feisonlei@gmail.com>
2018-06-13 01:39:21 +0800

    1.add create blog interface

--- handlers.py
+++ handlers.py
@@ -198,3 +198,12 @@ def manage_blogs(*, page='1'):
 		'__template__': 'manage_blogs.html',
 		'page_index': get_page_index(page)
 	}
+
+@get('/manage/blogs/create')
+def manage_create_blog():
+	return {
+		'__template__': 'manage_blog_edit.html',
+		'id':'',
+		'actions':'/api/blogs'
+	}
+
